e525f6f309a738945d3263e3d137d14783b6fabb
Wilson Jimenez <wilsonhjimenez@gmail.com>
2017-02-04 00:38:34 +0000

    changing forecast get request to https

--- src/actions.js
+++ src/actions.js
@@ -5,7 +5,7 @@ export const fetchSuccess = data => ({ type: 'FETCH_SUCCESS', data: data });
 export const fetchFail = err => ({ type: 'FETCH_FAIL', data: err });
 
 export const fetchData = query => dispatch => {
-	return axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${query}&mode=json&units=metric&appid=8da603b236dde573722ae04b5a16447b`)
+	return axios.get(`//api.openweathermap.org/data/2.5/forecast?q=${query}&mode=json&units=metric&appid=8da603b236dde573722ae04b5a16447b`)
   	.then(json => {
   		dispatch(fetchSuccess(json.data.list)),
   		dispatch(setCity(`${json.data.city.name}, ${json.data.city.country}`));
